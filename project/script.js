fd86722ec336f248a055c22287b239f06bf66818
Choi Choi <Lehungkhonghoc@gmail.com>
2026-01-21 22:28:15 +0700

    Add hello button with alert message

--- project/script.js
+++ project/script.js
@@ -1,2 +1,7 @@
 // Assignment 1 - Le Van Hung
 console.log("Project initialized");
+
+// Nút "Xin chào"
+document.getElementById('btnHello').addEventListener('click', function() {
+    alert('Xin chào');
+});
